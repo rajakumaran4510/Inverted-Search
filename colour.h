c75b7fc0d1d6e2305c1ef53f365b8827e923281b
Rajakumaran R <rajakumaran4510@gmail.com>
2026-02-15 11:36:06 +0530

    Add colour.h with color definitions

---
+++ colour.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define VIOLET   "\x1b[35m"
+#define RED_RED  "\033[31;41m"
+#define RESET   "\x1b[0m"
+#endif
